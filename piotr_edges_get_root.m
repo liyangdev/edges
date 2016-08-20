f098731564f420284290b3384f60df1fad8dd03f
Yang LI <liyangdev@gmail.com>
2016-08-20 14:11:06 +0800

    chore: add helper function

---
+++ piotr_edges_get_root.m
@@ -0,0 +1,7 @@
+function root_dir = piotr_edges_get_root()
+    % Return the root directory of the Piotr edges
+    %
+    % Author: LI Yang <liyangdev@gmail.com>
+    %
+    root_dir = fileparts(mfilename('fullpath'));
+end
